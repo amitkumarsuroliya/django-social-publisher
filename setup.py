d7c5c83d591ab08384d015e5300b348bc6989d35
Alexey <lisikhin@gmail.com>
2014-04-15 22:03:31 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DjangoSocialPublisher',
+    version='0.0.1dev',
+    packages=['social_publisher',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
