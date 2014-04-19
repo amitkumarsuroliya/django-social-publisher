056d657846ffb6d28174478c90ab42b267812611
Alexey <lisikhin@gmail.com>
2014-04-19 15:25:32 +0900

    Small code structure refactoring

--- social_publisher/mixins.py
+++ social_publisher/mixins.py
@@ -1,5 +1,5 @@
 import vkontakte
-from social_publisher.utils import upload_file_to_vk_wall
+from social_publisher.utils.vkontakte import publish_to_vk_wall
 
 
 class SocialPublisher(object):
@@ -7,11 +7,8 @@ class SocialPublisher(object):
     image_path = ''
 
     def publish(self):
-        self.publish_to_vk_wall()
+        self.publish_to_vk()
 
-    def publish_to_vk_wall(self):
+    def publish_to_vk(self):
         vk = vkontakte.API(token=self.vk_access_token)
-        photo = upload_file_to_vk_wall(vk, self.image_path)
-        attachments = photo[0]['id']
-        attachments += ',http://google.com'
-        vk.get('wall.post', message='Test message', attachments=attachments)
+        publish_to_vk_wall(vk, self.image_path)
--- social_publisher/utils/vkontakte.py
+++ social_publisher/utils/vkontakte.py
@@ -2,10 +2,19 @@ import json
 import requests
 
 
-def upload_file_to_vk_wall(vk, file_path):
+def publish_to_vk_wall(vk, image_path):
+    photo = upload_photo_to_vk_wall(vk, image_path)
+    attachments = photo[0]['id']
+    attachments += ',http://google.com'
+    vk.get('wall.post', message='Test message', attachments=attachments)
+
+
+def upload_photo_to_vk_wall(vk, file_path):
     upload_server = vk.get('photos.getWallUploadServer')
     files = {'photo': open(file_path, 'rb')}
     result = requests.post(upload_server['upload_url'], files=files)
     json_result = json.loads(result.text, strict=False)
     return vk.get('photos.saveWallPhoto', photo=json_result['photo'], server=json_result['server'],
                   hash=json_result['hash'])
+
+
